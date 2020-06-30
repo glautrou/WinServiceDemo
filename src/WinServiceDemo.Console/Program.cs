0bd038e8ed2646077c8749cf9460ed51a6f46891
Gilles Lautrou <g.lautrou@outlook.com>
2020-06-30 09:26:46 +0200

    Create empty solution/project referencing Topshelf library

---
+++ src/WinServiceDemo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinServiceDemo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
