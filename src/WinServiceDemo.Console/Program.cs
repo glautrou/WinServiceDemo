f21d5fbe8c5c86aecad14c563c485318308041dd
Gilles Lautrou <g.lautrou@outlook.com>
2020-06-30 10:05:46 +0200

    Implement Service Manager

--- src/WinServiceDemo.Console/Program.cs
+++ src/WinServiceDemo.Console/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using Topshelf;
 
 namespace WinServiceDemo.Console
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// It is used to set up the Windows Service.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Configure service host
+            var rc = HostFactory.Run(configure =>
+            {
+                //Service actions
+                configure.Service<ServiceManager>(s =>
+                {
+                    s.ConstructUsing(() => new ServiceManager());
+                    s.WhenStarted(i => i.Start());
+                    s.WhenStopped(i => i.Stop());
+                    s.WhenShutdown(i => i.Stop());
+                });
+
+                //Service settings
+                configure.RunAsLocalSystem();
+                configure.StartAutomatically();
+                configure.SetStopTimeout(TimeSpan.FromSeconds(10));
+                configure.OnException(ex => { System.Console.WriteLine($"Windows Service level exception: {ex.Message}"); });
+
+                //Service description
+                configure.SetServiceName("WinServiceDemo.Console");
+                configure.SetDisplayName("WinServiceDemo.Console");
+                configure.SetDescription("C# Windows Service demo using Topshelf");
+            });
+
+            //Exit code
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            System.Console.WriteLine($"Exit code: {exitCode}");
+            Environment.ExitCode = exitCode;
         }
     }
 }
---
+++ src/WinServiceDemo.Console/ServiceManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Timers;
+
+namespace WinServiceDemo.Console
+{
+    /// <summary>
+    /// Service manager containing service actions and managing processing.
+    /// </summary>
+    public class ServiceManager
+    {
+
+        /// <summary>
+        /// The timer interval
+        /// </summary>
+        private static readonly double TimerInterval = 10_000; //10 seconds
+        /// <summary>
+        /// The service timer
+        /// </summary>
+        private Timer _timer;
+
+        /// <summary>
+        /// Starts the servive
+        /// </summary>
+        public void Start()
+        {
+            //Initialize your service related instances here.
+            //Do not do it at service level otherwise ressources won't be properly disposed on stop/restart of the service.
+
+            System.Console.WriteLine("Starting service...");
+
+            _timer = new Timer(1_000);
+            _timer.Elapsed += Process;
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Stops the service
+        /// </summary>
+        public void Stop()
+        {
+            //Clean your ressources here
+
+            System.Console.WriteLine("Stopping service...");
+
+            _timer.Stop();
+            _timer.Elapsed -= Process;
+            _timer.Dispose();
+        }
+
+
+        /// <summary>
+        /// Process action for each tick
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="eventArgs">The <see cref="ElapsedEventArgs"/> instance containing the event data.</param>
+        private void Process(object sender, ElapsedEventArgs eventArgs)
+        {
+            _timer.Enabled = false; //Trick to avoid processes to overlap in case process duration longer than timer tick duration
+
+            System.Console.WriteLine("Processing...");
+
+            try
+            {
+                //Make sure there is no error thrown from the process method
+
+                //Simulate 1 second operation
+                System.Threading.Thread.Sleep(1_000);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+            System.Console.WriteLine("Processing... DONE!");
+
+            _timer.Interval = TimerInterval;
+            _timer.Enabled = true; //Unpause timer
+        }
+    }
+}
