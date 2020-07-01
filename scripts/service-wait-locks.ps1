0f377d0ae48eddee46f16d88bc02b0b47922287d
glautrou <g.lautrou@outlook.com>
2020-07-01 12:09:01 +0200

    Improve wait for locks command
    In some cases "[IO.File]::OpenWrite($file).close();" was causing an exception (but file not locked) and then waiting indefinitely.
    Now we simply try to rename file instead, and if the file does not exist (first time installation or previous deployment failure during copy) we skip this process

--- scripts/service-wait-locks.ps1
+++ scripts/service-wait-locks.ps1
@@ -1,22 +1,34 @@
 $file = "$(ServicePath)\log4net.dll"
 $isLocked = $true;
 try {
-    [IO.File]::OpenWrite($file).close();
+    # [IO.File]::OpenWrite($file).close();
+    # If the file cannot be renamed that means it is in use and locked
+    Rename-Item -Path "$file" -NewName "log4net.dll.old"
     $isLocked = $false;
 }
 catch {
     $isLocked = $true;
+    Write-Warning $Error[0]
 }
 
 while($isLocked)
 {
+    if (Test-Path $file) {
+        #File exists: neet to check if locked
+    } else {
+        # File missing, thus nothing should be locked
+        $isLocked = $false;
+    }
+
     Write-Host "File $(ServicePath)\log4net.dll is locked. Waiting for the lock to be released. Next try in 10 seconds..."
     Start-Sleep -s 10
     try {
-        [IO.File]::OpenWrite($file).close();
+        # [IO.File]::OpenWrite($file).close();
+        Rename-Item -Path "$file" -NewName "log4net.dll.old"
         $isLocked = $false;
     }
     catch {
         $isLocked = $true;
+        Write-Warning $Error[0]
     }
 }
